f56064ef0247474f823033ea3cc6d7c5cad8acb2
richie.zhu <richie.zhu@augmentum.com>
2021-05-17 17:09:14 +0800

    reverse partial linked list 92

--- ReverserPartialLinkedList92/Program.cs
+++ ReverserPartialLinkedList92/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoderCMS.Alogrithm.Common;
+using System;
 
 namespace ReverserPartialLinkedList92
 {
@@ -6,7 +7,40 @@ namespace ReverserPartialLinkedList92
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                        var method = "1";
+            if (args.Length == 1) 
+            {
+                method = args[0];
+            }
+
+            var listNumber = 10;
+            var left = 3;
+            var right = 8;
+
+            var testList = ListNodeUtil.InitAIntList(listNumber);
+
+            Console.WriteLine("Linked List before reverse:");
+            ListNodeUtil.PrintLinkedList(testList);
+
+            Solution solution = new Solution();
+
+            ListNode result = null;
+
+            switch (method)
+            {
+                case "1":
+                    result = solution.ReverseBetween(testList, left, right);
+                    break;
+                case "2":
+                    result = solution.ReverseBetween(testList, left, right);
+                    break;
+                default:
+                    result = solution.ReverseBetween(testList, left, right);
+                    break;
+            }
+
+            Console.WriteLine("Linked List after reverse:");
+            ListNodeUtil.PrintLinkedList(result);
         }
     }
 }
--- ReverserPartialLinkedList92/Solution.cs
+++ ReverserPartialLinkedList92/Solution.cs
@@ -1,3 +1,6 @@
+using CoderCMS.Alogrithm.Common;
+using System;
+
 namespace ReverserPartialLinkedList92
 {
     /// <summary>
@@ -5,9 +8,75 @@ namespace ReverserPartialLinkedList92
     /// </summary>
     public class Solution
     {
+        ListNode lastNodesNext = null;
+
+        /// <summary>
+        /// Solution #1: Recursive.
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private ListNode ReverseN(ListNode head, int n) 
+        {
+            if(head == null) return head;
+
+            if (head.next == null || n == 1)
+            {
+                // Record last node's next node. (the n+1 node)
+                lastNodesNext = head.next;
+
+                return head;
+            }
+
+            ListNode last = ReverseN(head.next, n - 1);
+
+            head.next.next = head;
+            head.next = lastNodesNext;
+
+            return last;
+        }
+
+        /// <summary>
+        /// Solution #2: iterative.
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private ListNode ReverseNTwo(ListNode head, int n)
+        {
+            ListNode current = head;
+            ListNode prev = null;
+
+            while (current != null && n > 0)
+            {
+                (current.next, prev, current) = (prev, current, current.next);
+
+                // var tmpCurrent = current;
+                // var tmpNext = current.next;
+
+                // current.next = prev;
+                // prev = tmpCurrent;
+                // current = tmpNext;
+
+                if (n == 1) 
+                {
+                    head.next = current;
+                }
+
+                n--;
+            }
+
+            return prev;
+        }
+
         public ListNode ReverseBetween(ListNode head, int left, int right) 
         {
-            return new ListNode();
+            if (left == 1)
+            {
+                return ReverseN(head,right);
+            }
+
+            head.next = ReverseBetween(head.next, left - 1, right - 1);
+
+            return head;
         }
     }
 }
