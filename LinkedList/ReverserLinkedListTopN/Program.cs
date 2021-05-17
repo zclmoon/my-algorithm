12aab796ca03d1ca14ce5d29b9ab0a6987e3fdc1
richie.zhu <richie.zhu@augmentum.com>
2021-05-17 17:52:56 +0800

    updated folder structure

---
+++ LinkedList/ReverserLinkedListTopN/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CoderCMS.Alogrithm.Common;
+
+namespace ReverserLinkedListTopN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = "1";
+            if (args.Length == 1) 
+            {
+                method = args[0];
+            }
+
+            var listNumber = 10;
+            var topN = 10;
+
+            var testList = ListNodeUtil.InitAIntList(listNumber);
+
+            Console.WriteLine("Linked List before reverse:");
+            ListNodeUtil.PrintLinkedList(testList);
+
+            Solution solution = new Solution();
+
+            ListNode result = null;
+
+            switch (method)
+            {
+                case "1":
+                    result = solution.ReverseN(testList, topN);
+                    break;
+                case "2":
+                    result = solution.ReverseNTwo(testList, topN);
+                    break;
+                default:
+                    result = solution.ReverseN(testList, topN);
+                    break;
+            }
+
+            Console.WriteLine("Linked List after reverse:");
+            ListNodeUtil.PrintLinkedList(result);
+        }
+    }
+}
